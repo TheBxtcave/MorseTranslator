a9908732696346a41dd5206024d1697ce544a5f2
Oliver Smith <siroliversmith@outlook.com>
2023-10-18 16:33:42 +0100

    commit

--- morse.py
+++ morse.py
@@ -1,108 +1,76 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+def english_to_morse(text):
+    morse_code_dict = {
+        'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+        'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+        'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+        'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+        'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+        'Z': '--..',
+        '0': '-----', '1': '.----', '2': '..---', '3': '...--',
+        '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+        '8': '---..', '9': '----.'
+    }
+    morse_code = []
+    for char in text.upper():
+        if char == ' ':
+            morse_code.append(' ')
+        elif char in morse_code_dict:
+            morse_code.append(morse_code_dict[char])
+        else:
+            morse_code.append(' ')
+    return ' '.join(morse_code)
 
-alphabet_to_morse = {
-    "A": ".-",
-    "B": "-...",
-    "C": "-.-.",
-    "D": "-..",
-    "E": ".",
-    "F": "..-.",
-    "G": "--.",
-    "H": "....",
-    "I": "..",
-    "J": ".---",
-    "K": "-.-",
-    "L": ".-..",
-    "M": "--",
-    "N": "-.",
-    "O": "---",
-    "P": ".--.",
-    "Q": "--.-",
-    "R": ".-.",
-    "S": "...",
-    "T": "-",
-    "U": "..-",
-    "V": "...-",
-    "W": ".--",
-    "X": "-..-",
-    "Y": "-.--",
-    "Z": "--..",
-    "0": "-----",
-    "1": ".----",
-    "2": "..---",
-    "3": "...--",
-    "4": "....-",
-    "5": ".....",
-    "6": "-....",
-    "7": "--...",
-    "8": "---..",
-    "9": "----.",
-    "Ä": ".-.-",
-    "Ü": "..--",
-    "ß": "...--..",
-    "À": ".--.-",
-    "È": ".-..-",
-    "É": "..-..",
-    ".": ".-.-.-",
-    ",": "--..--",
-    ":": "---...",
-    ";": "-.-.-.",
-    "?": "..--..",
-    "-": "-....-",
-    "_": "..--.-",
-    "(": "-.--.",
-    ")": "-.--.-",
-    "'": ".----.",
-    "=": "-...-",
-    "+": ".-.-.",
-    "/": "-..-.",
-    "@": ".--.-.",
-    "Ñ": "--.--",
-    " ": " ",
-    "" : ""
+
+def morse_to_english(morse_code):
+    reverse_morse_dict = {value: key for key, value in morse_code_dict.items()}
+    morse_words = morse_code.split(' ')
+    english_text = []
+    for word in morse_words:
+        if word == '':
+            english_text.append(' ')
+        elif word in reverse_morse_dict:
+            english_text.append(reverse_morse_dict[word])
+    return ''.join(english_text)
+
+
+# Dictionary to map characters to their respective Morse code
+morse_code_dict = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+    'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.'
 }
 
-morse_to_alphabet = {v: k for k, v in alphabet_to_morse.iteritems()}
+
+def main():
+    while True:
+        print("Select an option:")
+        print("1. Translate English to Morse code")
+        print("2. Translate Morse code to English")
+        print("3. Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            english_text = input("Enter the English text to translate to Morse code: ")
+            morse_code = english_to_morse(english_text)
+            print("Morse code:", morse_code)
+
+        elif choice == '2':
+            morse_code = input("Enter the Morse code to translate to English: ")
+            english_text = morse_to_english(morse_code)
+            print("English text:", english_text)
+
+        elif choice == '3':
+            break
+
+        else:
+            print("Invalid choice. Please try again.")
 
 
-def removeunusablecharacters(uncorrected_string):
-    return filter(lambda char: char in alphabet_to_morse, uncorrected_string.upper())
-
-
-def encode(decoded):
-    morsestring = []
-
-    decoded = removeunusablecharacters(decoded)
-    decoded = decoded.upper()
-    words = decoded.split(" ")
-    for word in words:
-        letters = list(word)
-
-        morseword = []
-        for letter in letters:
-            morseletter = alphabet_to_morse[letter]
-            morseword.append(morseletter)
-
-        word = "/".join(morseword)
-        morsestring.append(word)
-
-    return " ".join(morsestring)
-
-
-def decode(encoded):
-    characterstring = []
-
-    words = encoded.split(" ")
-    for word in words:
-        letters = word.split("/")
-
-        characterword = []
-        for letter in letters:
-            characterletter = morse_to_alphabet[letter]
-            characterword.append(characterletter)
-
-        word = "".join(characterword)
-        characterstring.append(word)
-
-    return " ".join(characterstring)
+if __name__ == "__main__":
+    main()
